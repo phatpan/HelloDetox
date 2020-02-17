ae87eed09d27c12fb115f95518a978314a31250d
Phatcharaphan ananpreechakun <phat.pan@gmail.com>
2020-02-17 17:07:03 +0700

    change test case

--- e2e/firstTest.spec.js
+++ e2e/firstTest.spec.js
@@ -3,17 +3,12 @@ describe('Example', () => {
     await device.reloadReactNative();
   });
 
-  it('should have welcome screen', async () => {
-    await expect(element(by.id('welcome'))).toBeVisible();
+  it('should show "Step One"', async () => {
+    await expect(element(by.text('Step One'))).toBeVisible();
   });
 
-  it('should show hello screen after tap', async () => {
-    await element(by.id('hello_button')).tap();
-    await expect(element(by.text('Hello!!!'))).toBeVisible();
+  it('should show "See Your Changes"', async () => {
+    await expect(element(by.text('See Your Changes'))).toBeVisible();
   });
 
-  it('should show world screen after tap', async () => {
-    await element(by.id('world_button')).tap();
-    await expect(element(by.text('World!!!'))).toBeVisible();
-  });
 });
